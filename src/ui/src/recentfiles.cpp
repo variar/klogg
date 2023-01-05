4e10b8717e681e548219b5d7c0824811e635ee45
Franciszek Koltuniuk <franciszek.koltuniuk@consult.red>
2023-01-05 13:10:35 +0100

    Move recent files to a separate submenu
    Changes:
     - move the list of recent files to a dedicated submenu
     - added an option to configure the number of recent files to list
     - added option to clean up the recent files list

--- src/ui/src/recentfiles.cpp
+++ src/ui/src/recentfiles.cpp
@@ -22,6 +22,7 @@
 
 #include "log.h"
 #include "recentfiles.h"
+#include "configuration.h"
 
 void RecentFiles::removeRecent( const QString& text )
 {
@@ -35,6 +36,11 @@ void RecentFiles::removeRecent( const QString& text )
     recentFiles_.removeAll( text );
 }
 
+void RecentFiles::removeAll()
+{
+    recentFiles_.clear();
+    recentFiles_.reserve( MAX_RECENT_FILES );
+}
 void RecentFiles::addRecent( const QString& text )
 {
 
@@ -45,8 +51,25 @@ void RecentFiles::addRecent( const QString& text )
     recentFiles_.push_front( text );
 
     // Trim the list if it's too long
-    while ( recentFiles_.size() > MAX_NUMBER_OF_FILES )
+    while ( recentFiles_.size() > MAX_RECENT_FILES )
         recentFiles_.pop_back();
+}
+
+int RecentFiles::getNumberItemsToShow() const
+{
+    return ( filesHistoryMaxItems_ < recentFiles_.count() ? filesHistoryMaxItems_ : recentFiles_.count() );
+}
+
+int RecentFiles::filesHistoryMaxItems() const
+{
+    return filesHistoryMaxItems_;
+}
+
+void RecentFiles::setFilesHistoryMaxItems( const int recentMaxFiles )
+{
+    if ( recentMaxFiles > 0 && recentMaxFiles <= MAX_RECENT_FILES ) {
+        filesHistoryMaxItems_ = recentMaxFiles;
+    }
 }
 
 QStringList RecentFiles::recentFiles() const
@@ -71,6 +94,7 @@ void RecentFiles::saveToStorage( QSettings& settings ) const
         settings.setValue( "name", recentFiles_.at( i ) );
     }
     settings.endArray();
+    settings.setValue( "maxMenuItems", filesHistoryMaxItems_ );
     settings.endGroup();
 }
 
@@ -78,12 +102,13 @@ void RecentFiles::retrieveFromStorage( QSettings& settings )
 {
     LOG_DEBUG << "RecentFiles::retrieveFromStorage";
 
-    recentFiles_.clear();
+    removeAll();
 
     if ( settings.contains( "RecentFiles/version" ) ) {
         settings.beginGroup( "RecentFiles" );
         if ( settings.value( "version" ).toInt() == RECENTFILES_VERSION ) {
             int size = settings.beginReadArray( "filesHistory" );
+            size = ( size < MAX_RECENT_FILES ) ? size : MAX_RECENT_FILES ;
             for ( int i = 0; i < size; ++i ) {
                 settings.setArrayIndex( i );
                 QString file = settings.value( "name" ).toString();
@@ -94,6 +119,7 @@ void RecentFiles::retrieveFromStorage( QSettings& settings )
         else {
             LOG_ERROR << "Unknown version of recent files, ignoring it...";
         }
+        setFilesHistoryMaxItems( settings.value( "maxMenuItems", DEFAULT_MAX_ITEMS_TO_SHOW ).toInt() );
         settings.endGroup();
     }
 }
