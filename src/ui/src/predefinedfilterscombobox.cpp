de6f7dc05fb6f54f224f99f91842c10a5ee6c0ce
chengzhl <chengzhl@cisco.com>
2023-06-22 17:47:54 +0800

    change format

--- src/ui/src/predefinedfilterscombobox.cpp
+++ src/ui/src/predefinedfilterscombobox.cpp
@@ -98,7 +98,7 @@ PredefinedFiltersComboBox::PredefinedFiltersComboBox( QWidget* parent )
     palette.setColor( QPalette::Base, palette.color( QPalette::Window ) );
     view()->setPalette( palette );
 
-    view()->setFixedWidth(200);
+    view()->setFixedWidth( 200 );
 }
 
 void PredefinedFiltersComboBox::populatePredefinedFilters()
