00b0029fc31540d31e8d7c83e67e21e82fe1f6de
Anton Filimonov <anton.filimonov@gmail.com>
2024-11-23 23:51:31 +0100

    fix: try manually copy libssl to appimage (#740)

--- packaging/linux/appimage/generate_appimage.sh
+++ packaging/linux/appimage/generate_appimage.sh
@@ -3,7 +3,13 @@
 DESTDIR=$(readlink -f appdir) ninja install
 wget -c -q "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+
 VERSION=$KLOGG_VERSION ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs
+
+mkdir -p appdir/usr/lib
+cp /lib/x86_64-linux-gnu/libssl* appdir/usr/lib
+
 VERSION=$KLOGG_VERSION ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
+
 mkdir ./packages
 cp ./klogg-$KLOGG_VERSION-x86_64.AppImage ./packages/klogg-$KLOGG_VERSION-x86_64.AppImage
