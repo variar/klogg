13778efa89233035fd20bdaf28101d7b845baa01
Anton Filimonov <anton.filimonov@gmail.com>
2024-11-23 23:44:24 +0100

    Revert "ci: fix appimage build"
    This reverts commit 12c5c3ca5e67bd350e7a88caf2c8b779aeb52a12.

--- packaging/linux/appimage/generate_appimage.sh
+++ packaging/linux/appimage/generate_appimage.sh
@@ -3,6 +3,6 @@
 DESTDIR=$(readlink -f appdir) ninja install
 wget -c https://github.com/$(wget -q https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous -O - | grep "appimagetool-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)
 chmod +x appimagetool-*.AppImage
-VERSION=$KLOGG_VERSION ARCH=x86_64 ./appimagetool-*.AppImage -s deploy appdir/usr/share/applications/*.desktop # Bundle EVERYTHING
+VERSION=$KLOGG_VERSION ./appimagetool-*.AppImage -s deploy appdir/usr/share/applications/*.desktop # Bundle EVERYTHING
 mkdir ./packages
 cp ./klogg-$KLOGG_VERSION-x86_64.AppImage ./packages/klogg-$KLOGG_VERSION-x86_64.AppImage
